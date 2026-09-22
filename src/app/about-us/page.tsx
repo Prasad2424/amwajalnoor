0fddffa1220f27e4981c9fe81efd05103f50d647
prasad-posina <123284863+Prasad2424@users.noreply.github.com>
2026-09-22 23:16:00 +0530

    Update page.tsx

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -15,7 +15,7 @@ const AboutUs = () => {
         className="max-w-7xl mx-auto px-6 py-20 space-y-24"
       >
         <h2 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-center text-black">
-          About Us
+          About Us...................
         </h2>
 
         {about.map((item, index) => (
